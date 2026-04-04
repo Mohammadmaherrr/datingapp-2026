96ad592e1a01a399850395c7956ad3c05272c900
Mohammadmaherrr <116066067+Mohammadmaherrr@users.noreply.github.com>
2026-04-04 15:58:57 +0300

    End of section 14

--- API/Entities/Member.cs
+++ API/Entities/Member.cs
@@ -21,6 +21,12 @@ public required string Country { get; set; }
 //Navigation property
 [JsonIgnore]
 public List<Photo> Photos { get; set; } = [];
+
+[JsonIgnore]
+public List<MemberLike> LikedByMembers { get; set; } = [];
+[JsonIgnore]
+public List<MemberLike> LikedMembers { get; set; } = [];
+
 [JsonIgnore]
 [ForeignKey(nameof(Id))]
 public AppUser User { get; set; } = null!;
--- client/src/core/services/init-service.ts
+++ client/src/core/services/init-service.ts
@@ -1,6 +1,7 @@
 import { inject, Injectable } from '@angular/core';
 import { AccountService } from './account-service';
 import { of } from 'rxjs';
+import { LikesService } from './likes-service';
 
 @Injectable({
   providedIn: 'root',
@@ -8,12 +9,14 @@ import { of } from 'rxjs';
 export class InitService {
 
   private accountService = inject(AccountService);
+  private likesService = inject(LikesService);
 
   init() {
     const userSting = localStorage.getItem('user');
     if (!userSting) return of(null);
     const user = JSON.parse(userSting);
     this.accountService.currentUser.set(user);
+    this.likesService.getLikeIds();
 
     return of(null)
   }
